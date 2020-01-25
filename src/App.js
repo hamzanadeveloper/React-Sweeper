9a9c37915242cf4b26ad1023a017ef8da6854d80
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2020-01-25 17:10:33 -0500

    Generated a bomb array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [bombs, setBombs] = useState([])
+
+  const generateBombs = () => {
+    console.log("Generating bombs")
+    let bombArr = Array(10).fill(0).map(elem => Array(10).fill(0))
+    for(let i = 0; i < bombArr.length; i++){
+      let bombPos = Math.floor(Math.random() * 10)
+      bombArr[i][bombPos] = 'X'
+    }
+    setBombs(bombArr)
+    console.log(bombs)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+          onClick={() => generateBombs()}
+        >Generate</button>
       </header>
     </div>
   );
