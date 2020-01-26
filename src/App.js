1f4599670c760c4d40cbf1b56f71e2be710b4c35
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2020-01-25 19:36:25 -0500

    Added DFS, lost game alert

--- src/App.js
+++ src/App.js
@@ -7,19 +7,32 @@ function App() {
   const buttonStyle = {
     width: 40,
     height: 40,
-    backgroundColor: '#777',
-    margin: 5,
+    backgroundColor: '#888',
     color: 'black',
     verticalAlign: 'top',
     fontSize: '32px',
     fontFamily: 'Arial, Helvetica, sans-serif;',
-    border: '1px solid #999',
+    borderLeft: '5px solid rgb(220,220,220)',
+    borderTop: '5px solid rgb(220,220,220)',
+    borderBottom: '5px solid #333',
+    borderRight: '5px solid #333',
+    display: 'inline-block'
+  }
+  const visitStyle = {
+    width: 50,
+    height: 50,
+    backgroundColor: '#444',
+    color: 'black',
+    verticalAlign: 'top',
+    fontSize: '32px',
+    fontFamily: 'Arial, Helvetica, sans-serif;',
     display: 'inline-block'
   }
 
   const generateBombs = () => {
     console.log("Generating bombs")
     let bombArr = Array(10).fill(0).map(elem => Array(10).fill(0))
+
     for(let i = 0; i < bombArr.length; i++){
       let bombPos = Math.floor(Math.random() * 10)
       bombArr[i][bombPos] = 'X'
@@ -50,8 +63,24 @@ function App() {
   }
 
   const visitCell = (i, j) => {
+    if(bombs[i][j] == 'X') alert("You lost! Generate again!")
+    dfsCells(i, j)
     visited[i][j] = 1
     setVisited([...visited])
+  }
+
+  function dfsCells(i, j) {
+    if(i < 0 || i > visited.length - 1 || j < 0 || j > visited[0].length - 1 || visited[i][j] == 1 || bombs[i][j] == 'X') return
+
+    visited[i][j] = 1
+
+    setVisited([...visited])
+    if(bombs[i][j] < 1){
+      dfsCells(i+1, j)
+      dfsCells(i-1, j)
+      dfsCells(i, j+1)
+      dfsCells(i, j-1)
+    }
   }
 
   return (
@@ -60,7 +89,11 @@ function App() {
         {bombs.map((arr, index) =>
           <div>
             {arr.map((elem, i) =>
-              <div onClick={() => visitCell(index, i)} style={buttonStyle}>{visited[index][i] == 0 ? null : bombs[index][i]}</div>
+              <div
+                onClick={() => visitCell(index, i)}
+                style={visited[index][i] == 0 ? buttonStyle : visitStyle}>
+                {visited[index][i] == 0 ? null : bombs[index][i] == 0 ? '' : bombs[index][i] }
+              </div>
             )}
           </div>
         )}
